443972196abffce550d154381f1d73b21cc00841
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-12 22:56:19 +0200

    correction de bug mineur

--- resources/views/private/accueil.blade.php
+++ resources/views/private/accueil.blade.php
@@ -25,8 +25,8 @@
 
 <!-- Affichage des différents profils -->
 <section class="colCenterContainer gap-y-10 bgPage mb-[21rem] min-[400px]:mb-68 md:mb-[30rem] lg:mb-[21rem] xl:mb-52">
-    <a href="192.168.1.250:2001" class="buttonForm">Gestionnaire de mot de passe</a>
-    <a href="192.168.1.250:2002" class="buttonForm">Tableau de bord des finances</a>
-    <a href="192.168.1.250:2003" class="buttonForm">Système de sauvegarde (Cloud)</a>
+    <a href="http://192.168.1.250:2001" class="buttonForm">Gestionnaire de mot de passe</a>
+    <a href="http://192.168.1.250:2002" class="buttonForm">Tableau de bord des finances</a>
+    <a href="http://192.168.1.250:2003" class="buttonForm">Système de sauvegarde (Cloud)</a>
 </section>
 @endsection
