4be6d269dfd0bcda5761b3d6dfd14c987eaf0aa8
FloRobart <FlorisRobart.pro@gmail.com>
2024-10-30 00:30:46 +0100

    correction d'un bug de redirection

--- app/Http/Middleware/VerifIP.php
+++ app/Http/Middleware/VerifIP.php
@@ -1,6 +1,10 @@
 <?php
-
 namespace App\Http\Middleware;
+
+/*
+ * Ce fichier fait partie du projet Home Server Maison
+ * Copyright (C) 2024 Floris Robart <florobart.github@gmail.com>
+ */
 
 use Closure;
 use Illuminate\Http\Request;
@@ -19,7 +23,7 @@ class VerifIP
         $ip = $request->ip();
         $adresseIP = AdresseIP::where('adresse_ip', $ip)->first();
         if ($adresseIP != null && $adresseIP->est_bannie == 1) {
-            return redirect()->route('private.accueil')->with('error', 'Votre adresse IP a été bannie, en continuant vous vous exposez à des poursuites judiciaires');
+            return redirect()->route('accueil')->with('error', 'Vous avez été bannie, en continuant vous vous exposez à des poursuites judiciaires');
         }
 
         return $next($request);
