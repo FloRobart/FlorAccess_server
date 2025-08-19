0cf1f9ad0a5d97ce11e6c21e658d27dcc128697f
FloRobart <FlorisRobart.pro@gmail.com>
2025-08-19 23:39:05 +0200

    Update users table and add trigger for update

--- Database/02-users.sql
+++ Database/02-users.sql
@@ -3,10 +3,32 @@ CREATE TABLE users (
     users_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     users_email VARCHAR(320) NOT NULL UNIQUE,
     users_name VARCHAR(255) NOT NULL,
-    users_password VARCHAR(255),
-    users_token VARCHAR(510),
-    users_ip VARCHAR(45),
-    users_authmode VARCHAR(255),
+    users_authmethod VARCHAR(255),
+
+    users_connected BOOLEAN DEFAULT FALSE,
+
+    users_password VARCHAR(512),
+    users_secret VARCHAR(512),
+    users_ip VARCHAR(512),
+
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (users_email, users_token)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- Function to update the updated_at field
+-- This function will be called by a trigger before each update
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to update the updated_at field on update
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
